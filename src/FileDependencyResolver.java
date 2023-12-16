309b6f79866b7a0db6ac057819228912fb194501
Drumik <drumik16@gmail.com>
2023-12-16 10:00:24 +0300

    Выявление зависимостей и сортировка по иерархии учитывая зависимости (без проверок цикличности)

--- src/FileDependencyResolver.java
+++ src/FileDependencyResolver.java
@@ -3,31 +3,61 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FileDependencyResolver {
 
    // private static LinkedList<String> sortedFiles = new LinkedList<>();
-
     public static void main(String[] args) {
         String rootFolder = "Root";
         File rootDirectory = new File(rootFolder);
         LinkedList <String> filesList = new LinkedList<>();
+        LinkedList <String> dependencies = new LinkedList<>();
 
         if (rootDirectory.exists()&&rootDirectory.isDirectory())
         {
             searchFiles(rootDirectory, filesList);
+
         }
+        System.out.println(rootDirectory.getName());
+        System.out.println(filesList);
 
-
-            concatenateFiles(filesList);
+        extractDependencies(rootDirectory,filesList,dependencies);
+        System.out.println(dependencies);
+        LinkedHashSet<String > set = new LinkedHashSet<>(dependencies);
+        dependencies.clear();
+        dependencies.addAll(set);
+        System.out.println(dependencies);
+        concatenateFiles(dependencies);
 
     }
-    private static void searchFiles(File directory, List<String>fileList)
+
+    private static void extractDependencies(File directory,List <String>filesList, List <String>dependencies){
+        int count = 0;
+        String root = directory.getName();
+        for (String item :filesList){
+            try (BufferedReader reader = new BufferedReader(new FileReader(filesList.get(count)))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Проверяем наличие директивы require и извлекаем путь к файлу
+                    if (line.startsWith("require")) {
+                        int startIndex = line.indexOf("'") + 1;
+                        int endIndex = line.lastIndexOf("'");
+                        if (startIndex != -1 && endIndex != -1) {
+                            String dependencyPath = root+"/"+ line.substring(startIndex, endIndex);
+                            dependencies.add(new File(dependencyPath).getAbsolutePath());
+                        }
+                    }
+                }
+                dependencies.add(new File(filesList.get(count)).getAbsolutePath());
+                count++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+        private static void searchFiles(File directory, List<String>fileList)
     {
         File [] files = directory.listFiles();
 
@@ -43,9 +73,8 @@ public class FileDependencyResolver {
             }
         }
     }
-
     private static void concatenateFiles(List<String> files) {
-        try (FileWriter outputWriter = new FileWriter("output.txt")) {
+        try (FileWriter outputWriter = new FileWriter("output")) {
             for (String file : files) {
                 try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                     String line;
@@ -54,7 +83,7 @@ public class FileDependencyResolver {
                     }
                 }
             }
-            System.out.println("Конкатенация успешно выполнена. Результат в файле output.txt");
+            System.out.println("Конкатенация успешно выполнена. Результат в файле output");
         } catch (IOException e) {
             e.printStackTrace();
         }
